ad7f34e21eb665b42a5a6f9bf9ee944b5d4f3d5c
somaxa8 <silviofrancoxa8@gmail.com>
2021-11-12 12:36:40 -0500

    refactor DocumentCategoryController

--- src/main/java/com/somacode/celmybell/controller/ResourceCategoryController.java
+++ src/main/java/com/somacode/celmybell/controller/ResourceCategoryController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-public class DocumentCategoryController {
+public class ResourceCategoryController {
 
     @Autowired
     ResourceCategoryService documentCategoryService;
